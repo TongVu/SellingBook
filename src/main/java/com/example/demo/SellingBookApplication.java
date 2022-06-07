16f9cf59bee3c4da36619d46ede41e2fc5b57e18
AAVN\tphvu <tongvu.244@gmail.com>
2022-06-07 08:36:38 +0700

    created Spring Boot project

---
+++ src/main/java/com/example/demo/SellingBookApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellingBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellingBookApplication.class, args);
+	}
+
+}
